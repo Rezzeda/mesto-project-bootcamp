823e415361dd7ee345cad5303e3d1d143c55ffbf
Reseda <pro@pro.local>
2023-12-08 15:41:20 +0400

    fixed edit profile popup, adding/deleting cards

--- index.js
+++ index.js
@@ -1,49 +1,62 @@
-// Редактирование профиля
+// popup Редактирование профиля
 const editProfileButton = document.querySelector('.profile__btn_action_edit-profile');
 const popup = document.querySelector('.popup');
-const popupEditProfile = document.querySelector('.popup_type_edit-profile') ;
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+const popupAddImage = document.querySelector('.popup_type_add-image');
 const nameInput = document.querySelector('.popup__item_name');
 const descriptionInput = document.querySelector('.popup__item_description');
-const closeButton = document.querySelector('.popup__btn_action_close');
+
 const submitButton = document.querySelector('.popup__btn_action_submit');
 const formElement = document.querySelector('.popup__form');
-const addCard = document.querySelector('.profile__btn_action_add-photos');
+
+// кнопка закрытия popup
+const closeButtons = document.querySelectorAll('.popup__btn_action_close');
+
+//popup добавление карточек
+const addCardButton = document.querySelector('.profile__btn_action_add-photos');
+const placeNameInput = document.querySelector('.profile__place_name');
+const itemLinkInput = document.querySelector('.profile__item_link');
 
 //Открытие модльного окна
-function openPopup () {
-    popup.classList.add('popup_opened');
+function openPopup (item) {
+    item.classList.add('popup_opened');
+}
+
+// добавляем обработчик события кнопке редактирования профиля для открытия модального окна c заполнением данных из профиля
+editProfileButton.addEventListener('click', () => {
+    openPopup(popupEditProfile);
     // Заполняем поля формы текущими значениями
     const currentName = document.querySelector('.profile__name').textContent;
     const currentDescription = document.querySelector('.profile__decription').textContent;
     // Устанавливаем значения в поля формы
     nameInput.value = currentName;
     descriptionInput.value = currentDescription;
-}
+});
 
 // Функция закрытия модального окна
-function closePopup() {
-    popup.classList.remove('popup_opened');
+function closePopup(item) {
+    item.classList.remove('popup_opened');
 }
 
-// Функция обновления профиля
+// функция закрытия попапа по нажатию на крестик
+closeButtons.forEach((item) => {
+    item.addEventListener('click', (evt) => {
+        const popupClosest = evt.target.closest('.popup');
+        closePopup(popupClosest);
+    });
+});
+
+// Функция обновления профиля введенными даннными
 function editProfile() {
     // Получаем значения из полей формы
     const newName = nameInput.value;
     const newDescription = descriptionInput.value;
-
     // Обновляем значения на странице
     document.querySelector('.profile__name').textContent = newName;
     document.querySelector('.profile__decription').textContent = newDescription;
-
     // Закрываем модальное окно
-    closePopup();
+    closePopup(popupEditProfile);
 }
-
-// добавляем обработчик события кнопке редактирования профиля для открытия модального окна
-editProfileButton.addEventListener('click', openPopup);
-
-// Находим кнопку закрытия и добавляем обработчик события для закрытия модального окна
-closeButton.addEventListener('click', closePopup);
 
 // Находим кнопку "Сохранить" и добавляем обработчик события для сохранения изменений
 submitButton.addEventListener('click', editProfile);
@@ -56,8 +69,10 @@ function handleFormSubmit(evt) {
 // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
 
-// добавляем обработчик события кнопке добавления карточки для открытия модального окна
-// addCard.addEventListener('click', openPopup')
+//добавляем обработчик события кнопке добавления карточки для открытия модального окна
+addCardButton.addEventListener('click', () => {
+    openPopup(popupAddImage);
+});
 
 // Добавление карточек «из коробки»
 const initialCards = [
@@ -86,3 +101,29 @@ const initialCards = [
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
+
+const templateCard = document.querySelector('#card-template').content.querySelector('.card');
+const listGalleryCards = document.querySelector('.gallery__cards');
+
+//функция создания карточки
+function createCard (dataCard) {
+    const galleryCard = templateCard.cloneNode(true);
+    const cardElementTitle = galleryCard.querySelector('.card__title');
+    const cardElementImage = galleryCard.querySelector('.card__image');
+    const cardElementTrashButton = galleryCard.querySelector('.card__btn_trash')
+    const cardElementLikeButton = galleryCard.querySelector('.card__btn');
+    cardElementTitle.textContent = dataCard.name;
+    cardElementImage.src = dataCard.link;
+    cardElementTrashButton.addEventListener('click', deleteCard);
+    return galleryCard;
+}
+
+//функция добавления карточек из массива
+initialCards.forEach((dataCard) => {
+    listGalleryCards.append(createCard(dataCard));
+});
+
+//Функция удаления карточки
+function deleteCard(evt) {
+    evt.target.closest('.card').remove();
+};
